71a0e34b9e76c1f4fd1f37bad226e698a19ed1f8
Polands in the Sky <glugglugglug@vivaldi.net>
2018-07-04 12:50:36 +0800

    u wot

--- bot.py
+++ bot.py
@@ -57,17 +57,29 @@ async def redditpopular(ctx,arg):
         embed=discord.Embed(title=submission.title,description=submission.selftext,color=embedcolor)
         embed.add_field(name="Points",value=submission.score)
         await bot.say(embed=embed)
-
+#info
 @bot.command(pass_context=True)
 async def info(ctx, user: discord.Member):
-    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color=embedcolor)
-    embed.add_field(name="Name", value=user.name, inline=True)
-    embed.add_field(name="ID", value=user.id, inline=True)
-    embed.add_field(name="Status", value=user.status, inline=True)
-    embed.add_field(name="Highest role", value=user.top_role)
-    embed.add_field(name="Joined", value=user.joined_at)
+    embed = discord.Embed(title="{}\'s info".format(user.display_name),description="Hi",color=embedcolor)
+    embed.add_field(name="User ID",value="{}".format(user.id),inline=True)
+    embed.add_field(name="Status",value="{}".format(user.status),inline=True)
+    embed.add_field(name="User's highest role",value="{}".format(user.top_role),inline=True)
+    embed.add_field(name="Date of joining the server",value="{}".format(user.joined_at))
     embed.set_thumbnail(url=user.avatar_url)
     await bot.say(embed=embed)
+    print("User has requested the info of {}.".format(user.name))
+
+@bot.command(pass_context=True)
+async def serverinfo(ctx):
+    embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Here's what I could find.", color=embedcolor)
+    embed.set_author(name="Maki")
+    embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Members", value=len(ctx.message.server.members))
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)
+    print("User has requested server info.")
 
 
 bot.run(secret['token'])
