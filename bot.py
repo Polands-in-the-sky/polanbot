344ead5422f97406580b954e37f79c3333d62519
Pölen Swarbrick <glugglugglug@vivaldi.net>
2018-08-05 11:37:41 +0800

    Memes

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ async def info(ctx, user: discord.Member):
 @bot.command(pass_context=True)
 async def serverinfo(ctx):
     embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Here's what I could find.", color=embedcolor)
-    embed.set_author(name="Maki")
+    embed.set_author(name="polanbot")
     embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
     embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
     embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
--- modules/games.py
+++ modules/games.py
@@ -1,5 +1,6 @@
 import asyncio
 import rethinkdb
 import discord
-from disc ord.ext import commands
+from discord.ext import commands
 from discord.ext.commands import Bot
+
